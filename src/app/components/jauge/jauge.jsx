e1d1c2f546af54aedaa27b6cca250cd36a589486
lightGriffith <light.griffith@gmail.com>
2018-09-29 21:11:04 +0200

    test mdp

--- src/app/components/jauge/jauge.jsx
+++ src/app/components/jauge/jauge.jsx
@@ -1,1 +1,19 @@
 import React, { Component } from 'react';
+
+export default class MapContainer extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+
+    componentWillMount() {
+    }
+
+
+    render() {
+        return (
+            
+        );
+    }
+}
